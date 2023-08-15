dc64e0e50be84fae66a9ab7939fc2c5887219a1d
Tri Nguyen <tri.m.nguyen@aalto.fi>
2023-08-15 15:20:14 +0200

    update Local outlier factor

--- modules/observation/streamAnalysis/functions.py
+++ modules/observation/streamAnalysis/functions.py
@@ -30,24 +30,27 @@ def detect_isolation_forest(data, feature, random_seed = 1, contamination = 0.05
         traceback.print_exception(*sys.exc_info())
         return None
     
-def train_local_outlier_factor(historical_data, n_neighbors = 20):
+def train_local_outlier_factor(historical_data, feature, n_neighbors = 2, novelty=True):
     # Train Local Ourlier Factor model
     try:
-        model_lof = LocalOutlierFactor(n_neighbors=n_neighbors)
-        model_lof.fit(historical_data)
+        model_lof = LocalOutlierFactor(n_neighbors=n_neighbors, novelty=novelty)
+        model_lof.fit(historical_data[feature].values)
         return model_lof
     except Exception as e:
         logging.error("Error {} while training isolation forest: {}".format(type(e),e.__traceback__))
         traceback.print_exception(*sys.exc_info())
         return None
 
-def detect_local_outlier_factor(model_lof, stream_data):
+def detect_local_outlier_factor(data, feature, n_neighbors = 2, novelty=True):
     # Detect outlier in stream data
     try:
+        model_lof = train_local_outlier_factor(data, feature, n_neighbors, novelty)
         """return 1: inlier; return -1: outlier"""
-        return (model_lof.predict(stream_data))
+        data['scores']=model_lof.decision_function(data[feature].values)
+        data['anomaly']=model_lof.predict(data[feature].values)
+        return data, model_lof
     except Exception as e:
-        logging.error("Error {} while predicting isolation forest: {}".format(type(e),e.__traceback__))
+        logging.error("Error {} while predicting with local outlier factor: {}".format(type(e),e.__traceback__))
         traceback.print_exception(*sys.exc_info())
         return None
 
@@ -60,5 +63,17 @@ def sdn_detect_isolation_forest(data, feature):
             df[metric] = df[metric] - df[metric].shift(1, fill_value=0)
         df = df.iloc[1:]
         dfs[key], model = detect_isolation_forest(df, feature)
-        print(dfs[key])
+        logging.debug(dfs[key])
+    return dfs,  model
+
+def sdn_detect_local_outlier_factor(data, feature):
+    dfs = {k:v for k, v in data.groupby('switch')}
+    sdn_shift_feature = ["agg_byte_count", "agg_packet_count","flow_packet_count", "flow_byte_count"]
+    for key in dfs:
+        df = copy.deepcopy(dfs[key])
+        for metric in sdn_shift_feature:
+            df[metric] = df[metric] - df[metric].shift(1, fill_value=0)
+        df = df.iloc[1:]
+        dfs[key], model = detect_local_outlier_factor(df, feature)
+        logging.debug(dfs[key])
     return dfs,  model
