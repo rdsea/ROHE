62b6d57e06d52f2c6717e3a5da075fd6dc3508f1
vtn13042000 <thaonguyen.vuong.16@gmail.com>
2023-09-14 13:33:07 +0300

    change inference deployment file for running on arm architecture

--- examples/applications/NII/kube_deployment/inferenceService/docker_deployment/deploy.sh
+++ examples/applications/NII/kube_deployment/inferenceService/docker_deployment/deploy.sh
@@ -4,7 +4,7 @@
 python copy_relevant_files.py --conf=config.json
 
 # Build the Docker image
-docker build -t vtn13042000/nii_inference_service_intel:1.0.0 .
+docker build -t vtn13042000/nii_inference_service_dev:1.0.0 .
 
 # Run the Python script to delete relevant files
 python delete_relevant_files.py --conf=config.json
