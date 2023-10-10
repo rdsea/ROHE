9d45ed49ac464ee5a7bb7630a3755db3ee59fee8
Tri Nguyen <tri.m.nguyen@aalto.fi>
2023-10-10 23:54:42 +0300

    udpate orchestration

--- lib/modules/orchestration/service/orchestration.py
+++ lib/modules/orchestration/service/orchestration.py
@@ -214,24 +214,3 @@ class RoheOrchestrationService(Resource):
             args = request.get_json(force=True)
         # get param from args here
         return jsonify({'status': args})
-
-if __name__ == '__main__': 
-    # init_env_variables()
-    parser = argparse.ArgumentParser(description="Argument for Rohe Orchestration Service")
-    parser.add_argument('--port', help='server port', default=5002)
-    parser.add_argument('--conf', help='configuration file', default=None)
-    parser.add_argument('--path', help='default config path', default="/configurations/orchestration/orchestrationConfig.json")
-    args = parser.parse_args()
-    config_file = args.conf
-    config_path = args.path
-    port = args.port
-    if not config_file:
-        config_file = qoaUtils.get_parent_dir(__file__,2)+config_path
-        print(config_file)
-    configuration = qoaUtils.load_config(config_file)
-    rohe_agent = RoheOrchestrationAgent(configuration,False)
-    configuration["agent"] = rohe_agent
-    
-
-    api.add_resource(RoheOrchestrationService, '/management',resource_class_kwargs=configuration)
-    app.run(debug=True, port=port)
