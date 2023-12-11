85d4342eb29b7ba92737d95d7719d5a8d788fd96
Tri Nguyen <tri.m.nguyen@aalto.fi>
2023-12-11 12:47:40 +0200

    deregister service

--- app/object_classification/services/test_services/reset_consul.py
+++ app/object_classification/services/test_services/reset_consul.py
@@ -1,3 +1,25 @@
-import sys, os
+import sys, os, argparse
 ROHE_PATH = os.getenv("ROHE_PATH")
 sys.path.append(ROHE_PATH)
+
+import lib.roheUtils as roheUtils
+
+from lib.serviceRegistry.consul import ConsulClient
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Argument for Ingestion Service")
+    parser.add_argument('--conf', type= str, help='specify configuration file path', 
+                        default= 'resetConsult.yaml')
+    parser.add_argument('--id', type= str, help='specify service endpoint', 
+                        default= '')
+    
+    args = parser.parse_args()
+    config_file = args.conf
+    config = roheUtils.load_config(file_path= config_file)
+    consul_client = ConsulClient(config= config['service_registry']['consul_config'])
+    for service in config["service"]:
+        instance_list = consul_client.getAllServiceInstances(service)
+        for instance in instance_list:
+            result = consul_client.serviceDeregister(instance["ID"])
+            if result:
+                print("Service {}, instance {} deregister success".format(service, instance["ID"]))
