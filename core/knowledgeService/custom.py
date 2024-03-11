f2d2a8ec13013a9f2f0fae3b943f2f869b8de492
Tri Nguyen <tri.m.nguyen@aalto.fi>
2024-03-11 12:50:11 +0200

    update user-defined metric

--- core/knowledgeService/custom.py
+++ core/knowledgeService/custom.py
@@ -29,5 +29,19 @@ def estimate_accuracy(model_name,mdb_client, db_name, coll_name = None, window =
     results = collection.find(filter_criteria).sort(sort_criteria).limit(window)
     result_list = list(results)
     df = pd.DataFrame(result_list, columns=result_list[0].keys())
-    filtered_df = df[(df['accuracy'] == 1)]
+    filtered_df = df[(df["accuracy"] == 1)]
     return len(filtered_df)/len(df)
+
+def estimate_confidence(model_name,mdb_client, db_name, coll_name = None, window = 10000, accurate_only=False):
+    db = mdb_client[db_name]
+    collection = db[coll_name]
+    filter_criteria = {"model": model_name}
+    sort_criteria = [("timestamp", pymongo.ASCENDING)]
+    results = collection.find(filter_criteria).sort(sort_criteria).limit(window)
+    result_list = list(results)
+    df = pd.DataFrame(result_list, columns=result_list[0].keys())
+    if accurate_only:
+        return df["confidence"].mean()
+    else:
+        filtered_df = df[(df["accuracy"] == 1)]
+        return filtered_df["confidence"].mean()
