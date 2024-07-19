42defee0e28304f10c815139952fb1b12ab48a11
Tri Nguyen <tri.m.nguyen@aalto.fi>
2024-07-19 14:20:03 +0300

    update utils

--- src/rohe/common/rohe_utils.py
+++ src/rohe/common/rohe_utils.py
@@ -8,10 +8,12 @@ import sys
 import traceback
 import types
 from typing import Callable
-
+import datetime
 import numpy as np
 import requests
 import yaml
+import re
+import socket
 
 logging.basicConfig(
     format="%(asctime)s:%(levelname)s -- %(message)s", level=logging.INFO
@@ -270,3 +272,114 @@ def get_n_sample_in_range(n, start, end):
             random_numbers.append(num)
 
     return random_numbers
+
+
+def decode_binary_image(binary_encoded_object: bytes, dtype: np.dtype, shape: tuple):
+    """
+    this function decode an binary object into a numpy array
+    input:
+    - binary_encoded (bytes): binary file-like object
+    - dtype (np.dtype): data type of original array
+    - shape (tuple): shape of numpy array
+    output: a numpy array
+    """
+    try:
+        image = np.frombuffer(binary_encoded_object, dtype=dtype)
+        image = image.reshape(shape)
+    except:
+        image = None
+    return image
+
+def save_numpy_array(self, arr, file_path):
+    """
+    Saves a numpy array to a file at the specified file path.
+    """
+    np.save(file_path, arr)
+
+def convert_str_to_datetime(str_time: str, option: str = None):
+    time = datetime.strptime(str_time, "%Y-%m-%dT%H:%M:%SZ")
+    if option == "date_only":
+        # Extract day, month, and year
+        day = time.day
+        month = time.month
+        year = time.year
+        # Format as d-m-y
+        formatted_date = f"{day}-{month}-{year}"
+        return formatted_date
+    else:
+        return time
+    
+def get_current_utc_timestamp(option: str = None):
+    """
+    get current utc timestamp
+    either date only (option='date_only')
+    or both date and time (default)
+    """
+    if option == "date_only":
+        return datetime.utcnow().strftime("%d-%m-%y")
+    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def extract_file_extension(url):
+    """
+    Extracts the file extension from a URL or file path
+    """
+    # Regular expression to match the file extension
+    match = re.search(r"\.([^.\/]+)$", url)
+    if match:
+        return match.group(1)
+    else:
+        return ""
+
+
+def convert_str_to_tuple(str_obj) -> tuple:
+    return tuple(map(int, str_obj.split(",")))
+
+def get_local_ip():
+    try:
+        # The following line creates a socket to connect to an external site
+        # The IP address returned is the one of the network interface used for the connection
+        # '8.8.8.8' is used here as it's a public DNS server by Google
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
+    except Exception as e:
+        return "Unable to get IP: " + str(e)
+    
+
+def parse_time(time_str) -> int:
+    """
+    Parses time strings into seconds.
+    Supported formats: Xs (seconds), Xm (minutes), Xh (hours).
+    """
+    match = re.match(r"(\d+)([smh])", time_str)
+    if not match:
+        raise ValueError(f"Invalid time format: {time_str}")
+
+    value, unit = int(match.group(1)), match.group(2)
+    if unit == "s":
+        return value
+    elif unit == "m":
+        return value * 60
+    elif unit == "h":
+        return value * 3600
+
+
+def handle_service_query(consul_client, service_name, query_type, tags=None):
+    try:
+        if query_type == "all":
+            return consul_client.get_all_service_instances(service_name, tags)
+
+        if query_type == "one":
+            return consul_client.get_n_random_service_instances(service_name, tags, n=1)
+
+        if query_type == "quorum":
+            return consul_client.get_quorum_service_instances(service_name, tags)
+        
+        logging.error(f"Invalid query type: {query_type}")
+        return None
+    except Exception as e:
+        logging.error(f"Error in handle_service_query: {e}")
+        return None
+    
