a4f61f963b18dc33780469fc8aa34a4d276a89cf
Tri <minhtribk12@gmail.com>
2026-03-23 20:07:30 +0200

    chore(examples): remove legacy deployment templates and Dockerfile.inference
    Remove old BTS kube_deployment directory and per-app Dockerfile.inference
    files that were replaced by the new per-service Dockerfile pattern.

--- examples/applications/BTS/kube_deployment/build_data_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t rdsea/bts_data_proc:2.0 -f ./docker_data_proc/Dockerfile .
-docker rmi -f $(docker images -q --filter "dangling=true")
-docker push rdsea/bts_data_proc:2.0 
--- examples/applications/BTS/kube_deployment/docker_ml/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export OPENBLAS_CORETYPE=ARMV8
-source ~/.bashrc
-# vim a.txt
-# tail -f /dev/null
-python3 ./task.py
